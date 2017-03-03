80a928c90a87903f38efe1d4023e519301db576f
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-03-03 11:09:02 +0700

    Character Movement

---
+++ Assets/Scripts/GameComponents/Unit Controller/UnitMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitMovement : MonoBehaviour {
+
+    public float speed;
+
+    string direction;
+    Vector3 right = new Vector3(2f, 1f);
+    Vector3 down = new Vector3(1f, -2f);
+
+    // Use this for initialization
+    void Start () {
+        direction = "s";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(1f, -2f) * Time.deltaTime*speed);
+	}
+}
