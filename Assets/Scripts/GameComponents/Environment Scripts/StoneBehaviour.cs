73ac359ed61e5bf3afeb96995ace3b4f96961319
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-03-03 10:39:00 +0700

    Random Type of stone

---
+++ Assets/Scripts/GameComponents/Environment Scripts/StoneBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneBehaviour : MonoBehaviour {
+
+    public Sprite[] stoneSprites;
+    int healthPoint;
+	// Use this for initialization
+	void Start () {
+        RandomTypeOfStone();
+	}
+
+    void RandomTypeOfStone()
+    {
+        int spriteIndex = Random.Range(0, stoneSprites.Length);
+        GetComponent<SpriteRenderer>().sprite = stoneSprites[spriteIndex];
+        healthPoint = spriteIndex < 4 ? 3 : 6;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
