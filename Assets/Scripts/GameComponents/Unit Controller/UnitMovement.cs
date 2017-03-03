d439dcb258380eab41517d6e344a9e28dfb736dc
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-03-03 11:14:47 +0700

    Unit Move with direction

--- Assets/Scripts/GameComponents/Unit Controller/UnitMovement.cs
+++ Assets/Scripts/GameComponents/Unit Controller/UnitMovement.cs
@@ -6,7 +6,7 @@ public class UnitMovement : MonoBehaviour {
 
     public float speed;
 
-    string direction;
+    public string direction;
     Vector3 right = new Vector3(2f, 1f);
     Vector3 down = new Vector3(1f, -2f);
 
@@ -17,6 +17,13 @@ public class UnitMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(new Vector3(1f, -2f) * Time.deltaTime*speed);
+        if (direction == "r")
+            transform.Translate(right * Time.deltaTime * speed);
+        else if (direction == "l")
+            transform.Translate(right * Time.deltaTime * speed * -1);
+        else if (direction == "d")
+            transform.Translate(down * Time.deltaTime * speed);
+        else if (direction == "u")
+            transform.Translate(down * Time.deltaTime * speed * -1);
 	}
 }
