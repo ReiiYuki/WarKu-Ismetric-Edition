ee9ff249ca0eb414e64bc89ed82b9324132b2e2f
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-03-07 10:40:26 +0700

    Set Position

---
+++ Assets/Scripts/GameComponents/Environment Scripts/BoardManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour {
+
+    GameObject[,] boardFloorObject, boardUnitObject;
+    Vector3[,] boardPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public void SetPosition(GameObject[,] boardFloorObject,GameObject[,] boardUnitObject,Vector3[,] boardPosition)
+    {
+        this.boardFloorObject = boardFloorObject;
+        this.boardUnitObject = boardUnitObject;
+        this.boardPosition = boardPosition;
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public Vector3 GetPosition(int x,int y)
+    {
+        return boardPosition[x, y];
+    }
+}
--- Assets/Scripts/GameComponents/Unit Controller/UnitMovement.cs
+++ Assets/Scripts/GameComponents/Unit Controller/UnitMovement.cs
@@ -6,20 +6,86 @@ public class UnitMovement : MonoBehaviour {
 
     public float speed;
     public string direction;
-    public int x,y;
-   
+    public int offset;
+    public int x,y,targetX,targetY;
+
+    BoardManager boardManager;
+
     Vector3 right = new Vector3(2f, 1f);
     Vector3 down = new Vector3(2f, -1f);
 
     // Use this for initialization
     void Start () {
         direction = "s";
-        transform.position = GameObject.FindGameObjectWithTag("SceneManager").GetComponent<SceneGanerator>().boardPosition[x, y];
-	}
+        boardManager = GameObject.FindGameObjectWithTag("SceneManager").GetComponent<BoardManager>();
+        transform.position = boardManager.GetPosition(x, y);
+    }
 	
+    void Move() 
+    {
+        if (y < targetY)
+        {
+            int nextY = y + 1;
+            if (transform.position.x < boardManager.GetPosition(x, nextY).x)
+            {
+                direction = "d";
+            }else
+            {
+                y = nextY;
+                transform.position = boardManager.GetPosition(x, nextY);
+            }
+        }else if (y > targetY)
+        {
+            int nextY = y - 1;
+            if (transform.position.x > boardManager.GetPosition(x, nextY).x)
+            {
+                direction = "u";
+            }
+            else
+            {
+                y = nextY;
+                transform.position = boardManager.GetPosition(x, nextY);
+            }
+        }
+        else
+        {
+            if (x < targetX)
+            {
+                int nextX = x + 1;
+                if (transform.position.x > boardManager.GetPosition(nextX, y).x)
+                {
+                    direction = "l";
+                }
+                else
+                {
+                    x = nextX;
+                    transform.position = boardManager.GetPosition(nextX, y);
+                }
+            }
+            else if (x > targetX)
+            {
+                int nextX = x - 1;
+                if (transform.position.x < boardManager.GetPosition(nextX, y).x)
+                {
+                    direction = "r";
+                }
+                else
+                {
+                    x = nextX;
+                    transform.position = boardManager.GetPosition(nextX, y);
+                }
+            }
+            else
+            {
+                direction = "s";
+                transform.position = boardManager.GetPosition(targetX, targetY);
+            }
+        }
+    }
+
 	// Update is called once per frame
 	void Update () {
-        transform.position = GameObject.FindGameObjectWithTag("SceneManager").GetComponent<SceneGanerator>().boardPosition[x, y];
+        Move();
         if (direction == "r")
             transform.Translate(right * Time.deltaTime * speed);
         else if (direction == "l")
@@ -29,4 +95,5 @@ public class UnitMovement : MonoBehaviour {
         else if (direction == "u")
             transform.Translate(down * Time.deltaTime * speed * -1);
 	}
+    
 }
