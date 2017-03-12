e115704d82b1ebccd6589723b0ec3360ba0d7973
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-03-12 22:46:50 +0700

    Zoom in zoom out by wheel

---
+++ Assets/Scripts/UIController/GameSceneUI/Zoomer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zoomer : MonoBehaviour {
+
+    public float scrollSpeed = 10f;
+    public float cameraDistance = 7f;
+    public float maxCameraDistance = 7f;
+    public float minCameraDistance = 3f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        cameraDistance -= Input.GetAxis("Mouse ScrollWheel") * scrollSpeed;
+        cameraDistance = Mathf.Clamp(cameraDistance, minCameraDistance, maxCameraDistance);
+        Debug.Log(cameraDistance);
+        Camera.main.orthographicSize = cameraDistance;
+    }
+}
