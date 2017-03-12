4ce31be74dce70751815ff64dd0e4a96f30a4185
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-03-12 23:13:00 +0700

    Zoom to specific area

--- Assets/Scripts/UIController/GameSceneUI/Zoomer.cs
+++ Assets/Scripts/UIController/GameSceneUI/Zoomer.cs
@@ -8,6 +8,7 @@ public class Zoomer : MonoBehaviour {
     public float cameraDistance = 7f;
     public float maxCameraDistance = 7f;
     public float minCameraDistance = 3f;
+    public bool onTile = false;
 
 	// Use this for initialization
 	void Start () {
@@ -16,9 +17,12 @@ public class Zoomer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        cameraDistance -= Input.GetAxis("Mouse ScrollWheel") * scrollSpeed;
-        cameraDistance = Mathf.Clamp(cameraDistance, minCameraDistance, maxCameraDistance);
-        Debug.Log(cameraDistance);
-        Camera.main.orthographicSize = cameraDistance;
+        if (Input.GetAxis("Mouse ScrollWheel") != 0 && onTile)
+        {
+            cameraDistance -= Input.GetAxis("Mouse ScrollWheel") * scrollSpeed;
+            cameraDistance = Mathf.Clamp(cameraDistance, minCameraDistance, maxCameraDistance);
+            Camera.main.orthographicSize = cameraDistance;
+            Camera.main.transform.position += (Camera.main.ScreenToWorldPoint(Input.mousePosition) - Camera.main.transform.position);
+        }
     }
 }
