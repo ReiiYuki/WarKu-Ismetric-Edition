b8576510c6541deaf625d3db100e5642090e4d27
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-03-28 11:18:56 +0700

    Separate-Client

---
+++ client/Assets/Scripts/Game/Board/BoardGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardGenerator : MonoBehaviour {
+
+    Board board;
+
+	// Use this for initialization
+	void Start () {
+        board = GetComponent<Board>();
+	}
+	
+    void PlaceRiver()
+    {
+        if (hasRiver())
+        {
+
+        }
+    }
+
+    bool hasRiver()
+    {
+        return Random.Range(0, 2) == 1;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
