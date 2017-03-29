4b249869f324b5db9eda4fea52615a7c9a9ff023
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-03-29 13:38:34 +0700

    Initialize Board Controller

---
+++ client/Assets/WarKu/Scripts/BoardController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardController : MonoBehaviour {
+
+    public GameObject[] tilePrototype;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
