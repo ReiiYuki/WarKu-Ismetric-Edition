08ab00dab5370da88b9ec63ac8f0e31f9ac136d6
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-03-29 23:22:08 +0700

    Add Offset on Unit

---
+++ client/Assets/WarKu/Scripts/GamePage/UnitBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitBehaviour : MonoBehaviour {
+
+    int x, y,targetX,targetY;
+    Vector3 offsetVector;
+	// Use this for initialization
+	void Start () {
+        offsetVector = new Vector3(0, GetComponent<SpriteRenderer>().sprite.bounds.size.y / 2);
+        transform.position += offsetVector;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
