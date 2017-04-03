feaed3ebb86af5f7b997811e6bd0d0a1c266aba6
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-04-03 20:35:24 +0700

    Update Hide Button in Tooltip

---
+++ client/Assets/WarKu/Scripts/GamePage/HideAction.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideAction : MonoBehaviour {
+
+    void OnMouseDown()
+    {
+        Debug.Log("Hide");
+    }
+}
