3a316917fbe9ff534a6360d0dc19c26c18a6f5b9
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-04-07 19:27:23 +0700

    Cancel Animation

---
+++ client/Assets/WarKu/Scripts/MainPage/CancelJoining.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CancelJoining : MonoBehaviour {
+
+    private void OnMouseDown()
+    {
+        transform.parent.GetComponent<Animator>().SetTrigger("Cancel");
+        GameObject.FindObjectOfType<CreateRoom>().Enable();
+    }
+}
--- client/Assets/WarKu/Scripts/MainPage/CreateRoom.cs
+++ client/Assets/WarKu/Scripts/MainPage/CreateRoom.cs
@@ -4,14 +4,27 @@ using UnityEngine;
 
 public class CreateRoom : MonoBehaviour {
 
+    private bool canCreate = true;
+
     private void OnMouseDown()
     {
-        Create();
+        if (canCreate) Create();
     }
 
     public void Create()
     {
         DGTProxyRemote.GetInstance().CreateRoom(0);
         GameObject.FindObjectOfType<ConnectionManager>().ShowCancel();
+    }
+
+    public void Enable()
+    {
+        StartCoroutine(ToEnable());
+    }
+
+    IEnumerator ToEnable()
+    {
+        yield return new WaitForSeconds(2f);
+        canCreate = true;
     }
 }
