cd689da24d701d26c056906bddf4b80bf272c6a2
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-04-09 07:37:48 +0700

    Notification Event

---
+++ client/Assets/WarKu/Scripts/GamePage/CloudBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudBehaviour : MonoBehaviour {
+
+    public Sprite[] clouds;
+
+    bool isFound = false;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<SpriteRenderer>().sprite = clouds[Random.Range(0, clouds.Length)];
+	}
+
+    void Update()
+    {
+        if (GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("End"))
+            gameObject.SetActive(false);
+        if (!isFound&&transform.parent.GetComponentInChildren<UnitBehaviour>())
+        {
+            isFound = true;
+            Found();
+        }
+    }
+
+	public void Found()
+    {
+        gameObject.SetActive(false);
+        Debug.Log("Found");
+        GetComponent<Animator>().SetTrigger("FadeOut");
+    }
+}
